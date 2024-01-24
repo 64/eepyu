a66893218e0afcb4c4aa8e77248e4f0014cb62c6
Matt Taylor <mstaveleytaylor@gmail.com>
2024-01-24 20:22:52 +0000

    Redesign pipe

--- eepyu/src/Core.scala
+++ eepyu/src/Core.scala
@@ -28,36 +28,44 @@ class Core(val imemWidth: Int = 4, val memWidth: Int = 8) extends Component {
   val INST = Payload(Bits(32 bits))
   val PC = Payload(UInt(imemWidth bits))
 
-  val fetch = Node()
-  val decode = Node()
-  val execute = Node()
-  val writeback = Node()
+  val pcStage, fetch, decode, execute, writeback = CtrlLink()
+  val pc2f = StageLink(pcStage.down, fetch.up)
+  val f2d = StageLink(fetch.down, decode.up)
+  val d2e = StageLink(decode.down, execute.up)
+  val e2w = StageLink(execute.down, writeback.up)
 
-  val f2d = StageLink(fetch, Node())
-  // val sf2d = S2MLink(f2d.down, Node())
-  val cf2d = CtrlLink(f2d.down, decode)
+  // val fetch = Node()
+  // val decode = Node()
+  // val execute = Node()
+  // val writeback = Node()
+  //
+  // val f2d = StageLink(fetch, Node())
+  // // val sf2d = S2MLink(f2d.down, Node())
+  // val cf2d = CtrlLink(f2d.down, decode)
+  //
+  // val d2e = StageLink(decode, Node())
+  // // val sd2e = S2MLink(d2e.down, Node())
+  // val cd2e = CtrlLink(d2e.down, execute)
+  //
+  // val e2w = StageLink(execute, Node())
+  // // val se2w = S2MLink(e2w.down, Node())
+  // val ce2w = CtrlLink(e2w.down, writeback)
 
-  val d2e = StageLink(decode, Node())
-  // val sd2e = S2MLink(d2e.down, Node())
-  val cd2e = CtrlLink(d2e.down, execute)
+  val pcArea = new pcStage.Area {
+    up.valid := True
 
-  val e2w = StageLink(execute, Node())
-  // val se2w = S2MLink(e2w.down, Node())
-  val ce2w = CtrlLink(e2w.down, writeback)
-
-  val fetchArea = new fetch.Area {
-    valid := fetchValid
-    fetchValid := True // hack ?
-    // io.mem.imemReadAddr := 0
-
-    val nextPc = pc + 4
-
-    when(isFiring) {
-      pc := nextPc
+    when(isValid && isReady) {
+      pc := pc + 4
     }
 
-    io.mem.imemReadAddr := isFiring ? nextPc | pc
     PC := pc
+
+    // Hack. The problem is that the PC stage doesn't hold the imemReadAddr when the fetch / decode stage has stalled.
+    // io.mem.imemReadAddr := isReady ? pc | RegNextWhen(pc, isReady)
+    io.mem.imemReadAddr := pc
+  }
+
+  val fetchArea = new fetch.Area {
     INST := io.mem.imemReadData.asBits
   }
 
@@ -72,11 +80,11 @@ class Core(val imemWidth: Int = 4, val memWidth: Int = 8) extends Component {
     val readsRs1 = decoder.io.rType || decoder.io.iType || decoder.io.sType || decoder.io.bType
     val readsRs2 = decoder.io.rType || decoder.io.sType || decoder.io.bType
 
-    def hazardInStage(stage: Node) = {
+    def hazardInStage(stage: CtrlLink) = {
       val nonZero = stage(DECODE).rd =/= 0
       val rs1Hazard = stage(DECODE).rd === decoder.io.rs1
       val rs2Hazard = stage(DECODE).rd === decoder.io.rs2
-        stage.isValid && nonZero && ((readsRs1 && rs1Hazard) || (readsRs2 && rs2Hazard))
+      stage.isValid && nonZero && ((readsRs1 && rs1Hazard) || (readsRs2 && rs2Hazard))
     }
     val rawHazardInExecute = hazardInStage(execute)
     val rawHazardInWriteback = hazardInStage(writeback)
@@ -84,7 +92,7 @@ class Core(val imemWidth: Int = 4, val memWidth: Int = 8) extends Component {
 
     when(anyHazard) {
       // report(Seq("e hazard: ", rawHazardInExecute, ", w hazard: ", rawHazardInWriteback))
-      cf2d.haltIt()
+      haltIt()
     }
   }
 
@@ -93,10 +101,15 @@ class Core(val imemWidth: Int = 4, val memWidth: Int = 8) extends Component {
     alu.io.op := DECODE.aluOp
     alu.io.en := True
     ALU_RESULT := alu.io.dst
-    ce2w.haltWhen(!alu.io.valid)
+    execute.haltWhen(!alu.io.valid)
 
     alu.io.src1 := 0
     alu.io.src2 := 0
+
+    val flush = False
+    for (stage <- List(pcStage, fetch, decode)) {
+      stage.throwWhen(flush, usingReady = true)
+    }
 
     when(DECODE.rType) {
       alu.io.src1 := regFile.io.rs1Data
@@ -116,15 +129,14 @@ class Core(val imemWidth: Int = 4, val memWidth: Int = 8) extends Component {
       alu.io.src1 := regFile.io.rs1Data
       alu.io.src2 := PC + DECODE.imm
 
-      when(isFiring) {
+      when(isValid) {
         // Set PC to branch target and flush pipe
         pc := alu.io.dst.resized
-        cf2d.throwIt()
-        fetchValid := False
-        fetch.valid := False
+        flush := True
 
         // Link register
         ALU_RESULT := (PC + 4).resized
+        report(Seq("jumping to PC ", pc))
       }
     }
 
@@ -132,13 +144,11 @@ class Core(val imemWidth: Int = 4, val memWidth: Int = 8) extends Component {
       alu.io.src1 := regFile.io.rs1Data
       alu.io.src2 := regFile.io.rs2Data
 
-      when(isFiring && alu.io.dst(0)) {
+      when(isValid && alu.io.dst(0)) {
         // hack
         pc := (PC + DECODE.imm).resized
-        // report(Seq("branching to PC ", pc))
-        cf2d.throwIt()
-        fetchValid := False
-        fetch.valid := False
+        flush := True
+        report(Seq("branching to PC ", pc))
       }
     }
 
@@ -162,22 +172,22 @@ class Core(val imemWidth: Int = 4, val memWidth: Int = 8) extends Component {
     // TODO: can we do this during execute stage?
     regFile.io.rd := DECODE.rd
     regFile.io.rdData := ALU_RESULT
-    regFile.io.writeValid := isFiring && (DECODE.rType || DECODE.iType || DECODE.jType || DECODE.uType)
-    io.error := RegNextWhen(True, isFiring && DECODE.error) init False
+    regFile.io.writeValid := isValid && (DECODE.rType || DECODE.iType || DECODE.jType || DECODE.uType)
+    io.error := RegNextWhen(True, isValid && DECODE.error) init False
 
     val order = Counter(64 bits)
 
-    when(isFiring) {
+    when(isValid) {
       order.increment()
     }
 
-    io.rvfi_valid := isFiring
+    io.rvfi_valid := isValid
     io.rvfi_insn := DECODE.inst.asUInt
     io.rvfi_order := order
     io.rvfi_halt := DECODE.error
   }
 
-  Builder(f2d, /* sf2d, */ cf2d, d2e, /* sd2e, */ cd2e, e2w, /* se2w, */ ce2w)
+  Builder(pcStage, pc2f, fetch, f2d, decode, d2e, execute, e2w, writeback)
 }
 
 object CoreVerilog extends App {
